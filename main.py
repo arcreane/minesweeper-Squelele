2377182265fa5b7dfd52a8368f4298cd8de4aab4
Squelele <elerig.gassien@outlook.fr>
2023-10-05 10:39:21 +0200

    test commit et affichage de départ de l'app

---
+++ main.py
@@ -0,0 +1,11 @@
+import playground
+
+print(45*"*")
+print(f"{'*':<44}*")
+print("* \t Welcome to this terminal Minesweeper\t*")
+print(f"{'*':<44}*")
+print(45*"*","\n")
+
+fieldWidth = input("Enter field width\n")
+fieldHeight = input("Enter field height\n")
+mines = input("How many mines ?\n")
